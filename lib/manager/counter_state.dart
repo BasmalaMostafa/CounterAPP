12a9463e9129eaf763de8623b04644a60b227c85
BasmalaMostafa <85370151+BasmalaMostafa@users.noreply.github.com>
2023-09-14 01:18:50 +0300

    test

--- lib/manager/counter_state.dart
+++ lib/manager/counter_state.dart
@@ -1,9 +1,13 @@
 part of 'counter_cubit.dart';
 
 
-class CounterState{
+class CounterState extends Equatable{
   int counterValue;
   bool? incremented;
 
   CounterState({required this.counterValue,incremented});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [counterValue,incremented];
 }
--- test/Cubit/cubit_counter_test.dart
+++ test/Cubit/cubit_counter_test.dart
@@ -11,30 +11,29 @@ void main() {
 
     setUp((){
       counterCubit = CounterCubit();
-    });
 
-    tearDown((){
-      counterCubit.close();
-    });
+      test('check if the initial state for the counter cubit is CounterState(counterValue:0)', () {
+        expect(counterCubit.state, CounterState(counterValue: 0));
+      });
 
-    test('check if the initial state for the counter cubit is CounterState(counterValue:0)', () {
-      expect(counterCubit.state, CounterState(counterValue: 0));
-    });
-
-    blocTest('check that the cubit should emit a counterState(counterValue:1 ,incremented:true) '
-        'when counterCubit.incremented() fun is called',
+      blocTest('check that the cubit should emit a counterState(counterValue:1 ,incremented:true) '
+          'when counterCubit.incremented() fun is called',
         build: ()=> counterCubit,
         act: (cubit) =>cubit.increment(),
-        expect: [CounterState(counterValue: 1,incremented: true)],
-    );
+        expect: ()=> CounterState(counterValue: 1,incremented: true),
+      );
 
+      blocTest('check that the cubit should emit a counterState(counterValue:-1 ,incremented:false) '
+          'when counterCubit.decremented() fun is called',
+        build: ()=> counterCubit,
+        act: (cubit) =>cubit.decrement(),
+        expect: ()=> CounterState(counterValue: -1,incremented: false),
+      );
 
-  blocTest('check that the cubit should emit a counterState(counterValue:-1 ,incremented:false) '
-      'when counterCubit.decremented() fun is called',
-    build: ()=> counterCubit,
-    act: (cubit) =>cubit.decrement(),
-    expect: [CounterState(counterValue: -1,incremented: false)],
-  );
+      tearDown((){
+        counterCubit.close();
+      });
+    });
   });
 }
 
