31ccbf33e486da48e7d577483d132db1a690a3a9
BasmalaMostafa <85370151+BasmalaMostafa@users.noreply.github.com>
2023-09-14 01:00:49 +0300

    testing

---
+++ test/Cubit/cubit_counter_test.dart
@@ -0,0 +1,53 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:counterapp/manager/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:counterapp/main.dart';
+
+void main() {
+  group('Counter Cubit Test', () {
+    CounterCubit counterCubit;
+
+    setUp((){
+      counterCubit = CounterCubit();
+    });
+
+    tearDown((){
+      counterCubit.close();
+    });
+
+    test('check if the initial state for the counter cubit is CounterState(counterValue:0)', () {
+      expect(counterCubit.state, CounterState(counterValue: 0));
+    });
+
+    blocTest('check that the cubit should emit a counterState(counterValue:1 ,incremented:true) '
+        'when counterCubit.incremented() fun is called',
+        build: ()=> counterCubit,
+        act: (cubit) =>cubit.increment(),
+        expect: [CounterState(counterValue: 1,incremented: true)],
+    );
+
+
+  blocTest('check that the cubit should emit a counterState(counterValue:-1 ,incremented:false) '
+      'when counterCubit.decremented() fun is called',
+    build: ()=> counterCubit,
+    act: (cubit) =>cubit.decrement(),
+    expect: [CounterState(counterValue: -1,incremented: false)],
+  );
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
